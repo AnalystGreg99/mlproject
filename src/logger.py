db7297c591deb00ac73de57c1173e81287b6d160
AnalystGreg99 <chinedum.ilobinso@stu.cu.edu.ng>
2024-11-15 01:04:10 +0100

    logging and exception corrected

--- src/logger.py
+++ src/logger.py
@@ -18,15 +18,3 @@ logging.basicConfig(
 
 
 )
-
-# Example logging to test 
-logger = logging.getLogger(__name__) 
-logger.info("Logging is set up correctly!")
-if __name__=="__main__":
-
-    try:
-        a=1/0
-
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
