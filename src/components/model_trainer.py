6c113a222b08cb6333fe2c7c81fcabdaa4ac86e6
Chinedum Emmanuel Ilobinso <139987611+AnalystGreg99@users.noreply.github.com>
2024-11-16 19:26:13 +0100

    Hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,22 +47,68 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor()
             }
+            
+            params = {
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [3, 5, 10],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    'leaf_size': [20, 30, 40]
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    #'splitter': ['best', 'random'],
+                    #'max_depth': [None, 10, 20, 30],
+                    #'min_samples_split': [2, 5, 10],
+                    #'min_samples_leaf': [1, 2, 4],
+                    #'max_features': ['auto', 'sqrt', 'log2']
+                },
+                "Random Forest Regressor": {
+                    'n_estimators': [100, 200, 500],
+                    #'criterion': ['squared_error', 'absolute_error', 'poisson'],
+                    #'max_depth': [None, 10, 20, 30],
+                    #'min_samples_split': [2, 5, 10],
+                    #'min_samples_leaf': [1, 2, 4],
+                    #'max_features': ['auto', 'sqrt', 'log2'],
+                    #'bootstrap': [True, False]
+                },
+                "XGBRegressor": {
+                    'n_estimators': [100, 200, 500],
+                    'learning_rate': [0.01, 0.1, 0.3]
+                },
+                "AdaBoost Regressor": {
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.1, 1.0],
+                    #'loss': ['linear', 'square', 'exponential']
+                },
+                "Gradient Boosting": {
+                    'n_estimators': [100, 200, 500],
+                    'learning_rate': [0.01, 0.1, 0.3],
+                    #'max_depth': [3, 5, 7],
+                    #'subsample': [0.6, 0.8, 1.0],
+                    #'max_features': ['auto', 'sqrt', 'log2']
+                }
+            }
 
             model_report: dict = evaluate_model(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                param=params
             )
 
             # Get best model score from dict
-            best_model_score = max(model_report.values())
+            best_model_score = max(sorted(model_report.values()))
 
-            # Get best model name from dict
-            best_model_name = max(model_report, key=model_report.get)
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
-
             if best_model_score < 0.6:
                 raise CustomException("No best model found with acceptable score")
 
